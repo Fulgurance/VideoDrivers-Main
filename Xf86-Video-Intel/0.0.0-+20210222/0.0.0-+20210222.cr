7ebb13fcd38ed39ef03cec90ea61c2ed0a3c77eb
fulgurance <zohran.londais@gmail.com>
2023-04-07 10:18:52 +0100

    Added video drivers installer

---
+++ Xf86-Video-Intel/0.0.0-+20210222/0.0.0-+20210222.cr
@@ -0,0 +1,30 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--localstatedir=/var",
+                            "--disable-static",
+                            "--enable-kms-only",
+                            "--enable-uxa",
+                            "--mandir=/usr/share/man"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        fileReplaceText("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/man/man1/intel-virtual-output.1",".TH intel-virtual-output  4",".TH intel-virtual-output  1")
+    end
+
+end
